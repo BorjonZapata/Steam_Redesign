1c81c65815deb33f316efcabc93000e7bd11fd4d
BorjonZapata <alejandro.borjon@protonmail.com>
2024-02-25 18:05:14 -0600

    Prueba de reinstalacion de computadora principal

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Probando cambios y funcionamiento de gitSHIT
+          Probando Github en computadora principal
         </p>
         <a
           className="App-link"
