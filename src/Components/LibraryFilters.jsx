c2d2b3fa8cc4a37cd04197208647adb438a9eae7
BorjonZapata <alejandro.borjon@protonmail.com>
2024-02-27 08:52:05 -0600

    Agergados los elementos de la lista filters

--- src/Components/LibraryFilters.jsx
+++ src/Components/LibraryFilters.jsx
@@ -4,11 +4,52 @@ function LibraryFilters(props) {
     return (
       <div className='noJumpDiv'>
         {props.isVisible ? (
-          <ul className='dropdown'>
-            <li>Option 1</li>
-            <li>Option 2</li>   
-            <li>Option 3</li>
-          </ul>
+          <dl className='dropdown'>
+            <div>
+              <dt>Type</dt>
+                <dd>Games</dd>
+                <dd>Software</dd>
+                <dd>Tools</dd>
+                <dd>Videos</dd>
+                <dd>Soundtracks</dd>
+              <dt>Palyers</dt>  
+                <dd>Single Player</dd> 
+                <dd>Multiplayer</dd>
+                <dd>Cooperative</dd>
+              <dt>Play State</dt>
+                <dd>Ready to Play</dd>
+                <dd>Instaled Locally</dd>
+                <dd>Played</dd>
+                <dd>Unplayed</dd>
+            </div>
+            <div>
+              <dt>Hardware Support</dt>
+                <dd>Controllers (Full)</dd>
+                <dd>Controllers (Partial)</dd>
+                <dd>VR</dd>
+              <dt>Features</dt>
+                <dd>Trading cards</dd>
+                <dd>Workshop</dd>
+                <dd>Achievements</dd>
+                <dd>Remote play Together</dd>
+              <dt>Friends</dt>
+              <dd><input type="text" placeholder="Enter Friend's Name"></input></dd>
+              <dt>Store Tags</dt>
+                <dd><input type="text" placeholder="Search for a tag"></input></dd>
+            </div>
+            <div>
+              <dt>Genre</dt>
+                <dd>Action</dd>
+                <dd>Adventure</dd>
+                <dd>Casual</dd>
+                <dd>Massively Multiplayer</dd>
+                <dd>Racing</dd>
+                <dd>RPG</dd>
+                <dd>Simulation</dd>
+                <dd>Sports</dd>
+                <dd>Strategy</dd>
+              </div>
+          </dl>
         ) : null}
       </div>
     );
