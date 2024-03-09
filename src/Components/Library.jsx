7d19b3d3ee90451f48a488b429a96ada6cad721c
BorjonZapata <alejandro.borjon@protonmail.com>
2024-03-08 19:09:55 -0600

    Repuesta recibida de pantry, lista de juegos impresa en la seccion de librería, idealmente seria hacer un componente para mostrar los datos con una mejor presentacion, pero no hay tiempo.

--- src/Components/Library.jsx
+++ src/Components/Library.jsx
@@ -2,11 +2,7 @@ import PlayerSummaries from "./PlayerSummaries";
 
 function Library() {
     return (
-        <div className="LibraryPlaceholder">
-            PlaceHolder Biblioteca. Los juegos van aquí, eventuelmente, cuando agregue Apis
             <PlayerSummaries></PlayerSummaries>
-
-        </div>
     );
   }
   export default Library;
--- src/Components/PlayerSummaries.jsx
+++ src/Components/PlayerSummaries.jsx
@@ -1,55 +1,33 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, {useState, useEffect} from 'react';
 
 function PlayerSummaries() {
-  axios.get('http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=413150&count=1&maxlength=300&format=json', {
-    headers: {
-      "Access-Control-Allow-Origin": "http://api.steampowered.com"
-    }
-  })
-  .then(response => {
-    console.log(response.data)
-  })
-  .catch(error => {
-    console.error(error)
-  });
-
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    axios.get('https://getpantry.cloud/apiv1/pantry/d16371a2-ef02-4822-81e1-59510e6f90ef/basket/Library')
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      })
+  }, []);
   return (
-    <div>
+    <div className='Games'>
+      {data && (
+        <div>
+          <p>Juegos:  {data.response.game_count}</p>
+          {data.response.games.map((game, index) => (
+            <div key={index}>
+              <p>ID del juego: {game.appid}</p>
+              <p>Tiempo de juego: {game.playtime_windows_forever}</p>
+              <p>-------------------------------</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
-{/* http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=413150&count=1&maxlength=300&format=json
-Puedo hacer un test de la respuesta de json en postman sin problemas, el resultado de json es este: {
-    "response": {
-        "players": [
-            {
-                "steamid": "76561198145120478",
-                "communityvisibilitystate": 3,
-                "profilestate": 1,
-                "personaname": "Roka",
-                "commentpermission": 1,
-                "profileurl": "https://steamcommunity.com/id/rokaalex/",
-                "avatar": "https://avatars.steamstatic.com/d7a74b14ff980fe1834e2b32ba4ddda87b226124.jpg",
-                "avatarmedium": "https://avatars.steamstatic.com/d7a74b14ff980fe1834e2b32ba4ddda87b226124_medium.jpg",
-                "avatarfull": "https://avatars.steamstatic.com/d7a74b14ff980fe1834e2b32ba4ddda87b226124_full.jpg",
-                "avatarhash": "d7a74b14ff980fe1834e2b32ba4ddda87b226124",
-                "lastlogoff": 1709303888,
-                "personastate": 1,
-                "primaryclanid": "103582791429521408",
-                "timecreated": 1405479487,
-                "personastateflags": 0,
-                "loccountrycode": "MX"
-            }
-        ]
-    }
-}
 
-Sin embargo, al inetentar hacer el fetch en mi servidor de prueba de react no se muestra la informacion del componennte, la terminal del navegador entrega el siguiente error:
-Cross-Origin Request Blocked: The Same Origin Policy disallows reading the remote resource at http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=63F3B72409107F526DEB08F5935180C1&steamids=76561198145120478.
-(Reason: CORS header ‘Access-Control-Allow-Origin’ missing).
-Segun lo que puedo investigar aqui: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS/Errors/CORSMissingAllowOrigin
-necesito hacer un cambio en la configuracion de mi servidor para que tenga la configuracion Access-Control-Allow-Origin: https://example.com de la direccion de la que quiero usar
-mi api, sin embargo no puedo averiguar como implementar esto en mi proyecto.
-*/}
 export default PlayerSummaries;
